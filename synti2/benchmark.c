827d881e01981b9ada51446ac274d29b59a54c8d
Paavo Nieminen <paavo.j.nieminen@jyu.fi>
2011-09-30 21:41:16 +0300

    Tentatively adding a benchmark-to-be.

---
+++ synti2/benchmark.c
@@ -0,0 +1,17 @@
+/** (Planned; TODO: implement this.) A benchmark test that runs the
+ * synthesizer as fast as it runs, and prints out the execution time.
+ * I'm going to use this for testing various approaches to the inner
+ * loop implementation and stuff...
+ * 
+ * Also a good playground for making a Wav-writer.
+ *
+ */
+
+#include "synti2.h"
+
+synti2_conts *global_cont;
+synti2_synth *st;
+
+int main(int argc, char **argv){
+  return 0;
+}

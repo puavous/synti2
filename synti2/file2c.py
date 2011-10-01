15948244a91a15e3d54519cab6840eaf97370da4
Paavo Nieminen <paavo.j.nieminen@jyu.fi>
2011-10-01 05:31:49 +0300

    Preparations for creating and testing the song player.

---
+++ synti2/file2c.py
@@ -0,0 +1,17 @@
+import sys,string
+
+infname = sys.argv[1]
+
+
+infile = file(infname, 'rb')
+data = bytearray(infile.read())
+
+length = len(data)
+
+print
+print "/* Song data converted from file '%s': */"%infname
+print "unsigned int %s_length = %d"%(sys.argv[2],length)
+print "unsigned char %s_data = {"%(sys.argv[2])
+print string.join(map(lambda x: "0x%02x,"%x ,  data))
+
+print "};"

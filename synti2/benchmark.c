2df36c3639c76ed0ceb54726afd86ae1e12529eb
Paavo Nieminen <paavo.j.nieminen@jyu.fi>
2011-10-01 18:56:09 +0300

    Typed in the benchmark thingy.

--- synti2/benchmark.c
+++ synti2/benchmark.c
@@ -7,11 +7,64 @@
  *
  */
 
+#include <stdlib.h>
+#include <stdio.h>
 #include "synti2.h"
 
-synti2_conts *global_cont;
-synti2_synth *st;
+/* How many seconds to run the synth. */
+#define BENCHMARK_SECONDS 10
 
-int main(int argc, char **argv){
+synti2_conts *global_cont;
+synti2_synth *global_synth;
+synti2_player *global_player;
+
+synti2_smp_t global_buffer[20000]; /* FIXME: limits? */
+
+/* Test patch from the hack script: */
+extern unsigned char hack_patch_sysex[];
+extern int hack_patch_sysex_length;
+
+/* And a test song: */
+extern unsigned char hacksong_data[];
+extern unsigned int hacksong_length;
+
+int main(int argc, char *argv[])
+{
+  int iframe;
+  unsigned int sr = 48000;
+  int frames_at_once = 128;
+  static int firsttime = 1;
+
+  /* My own soft synth to be created. */
+  global_synth = synti2_create(sr);
+  global_cont = synti2_conts_create();
+  if ((global_synth == NULL) || (global_cont == NULL)){
+    fprintf (stderr, "Couldn't allocate synti-kaksi \n");
+    exit(1);
+  };
+
+  /*hack. FIXME: remove.*/
+  global_player = synti2_player_create(hacksong_data, hacksong_length, sr);
+  if (global_player == NULL){
+    fprintf(stderr, "No player could be made \n");
+    exit(1);
+  }
+
+
+  for(iframe=0; iframe < sr * BENCHMARK_SECONDS; iframe += frames_at_once){
+    synti2_conts_reset(global_cont);
+    
+    if (firsttime != 0) {
+      firsttime = 0;
+      synti2_conts_store(global_cont, 0, 
+                         hack_patch_sysex, hack_patch_sysex_length);
+    } else {
+      /*  synti2_player_render(global_player, global_cont, nframes);*/
+    }
+    
+    synti2_conts_start(global_cont);
+    synti2_render(global_synth, global_cont,
+                  global_buffer, frames_at_once); 
+  }
   return 0;
 }

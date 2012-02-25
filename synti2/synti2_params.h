c70ebe7a8d30c4bd7e8ffe144770d12fb8d9d6cf
Paavo Nieminen <paavo.j.nieminen@jyu.fi>
2012-02-25 14:13:54 +0200

    Separated the parameter descriptions as a preparation to make them autogenerated.

---
+++ synti2/synti2_params.h
@@ -0,0 +1,67 @@
+#ifndef SYNTI2_PARAMETER_DESCRIPTIONS_H
+#define SYNTI2_PARAMETER_DESCRIPTIONS_H
+
+/* FIXME: This should be automatically generated!! */
+
+/* Three-bit integer parameters (range 0-7) */
+#define SYNTI2_I3_ELIFE 0
+#define SYNTI2_I3_EAMP1 1
+#define SYNTI2_I3_EAMP2 2
+#define SYNTI2_I3_EAMP3 3
+#define SYNTI2_I3_EAMP4 4
+#define SYNTI2_I3_EAMPN 5
+#define SYNTI2_I3_EPIT1 6
+#define SYNTI2_I3_EPIT2 7
+#define SYNTI2_I3_EPIT3 8
+#define SYNTI2_I3_EPIT4 9
+#define SYNTI2_I3_ELOOP1 10
+#define SYNTI2_I3_ELOOP2 11
+#define SYNTI2_I3_ELOOP3 12
+#define SYNTI2_I3_ELOOP4 13
+#define SYNTI2_I3_ELOOP5 14
+#define SYNTI2_I3_ELOOP6 15
+
+#define SYNTI2_I3_FMTO1 16
+#define SYNTI2_I3_FMTO2 17
+#define SYNTI2_I3_FMTO3 18
+#define SYNTI2_I3_FMTO4 19
+#define SYNTI2_I3_ADDTO1 20
+#define SYNTI2_I3_ADDTO2 21
+#define SYNTI2_I3_ADDTO3 22
+#define SYNTI2_I3_ADDTO4 23
+
+/*FIXME: We could have some wavetable info here! */
+
+#define SYNTI2_I3_NPARS 24
+
+/* Eight-bit integer parameters (range 0-127) */
+#define SYNTI2_I7_NPARS 16
+
+/*TODO: Design the patch format properly. */
+#define SYNTI2_F_ENVS 0
+
+#define SYNTI2_F_DT1 60
+#define SYNTI2_F_DT2 61
+#define SYNTI2_F_DT3 62
+#define SYNTI2_F_DT4 63
+#define SYNTI2_F_xxx 64
+#define SYNTI2_F_LV1 65
+#define SYNTI2_F_LV2 66
+#define SYNTI2_F_LV3 67
+#define SYNTI2_F_LV4 68
+#define SYNTI2_F_LVN 69
+#define SYNTI2_F_VS1 70
+#define SYNTI2_F_VS2 71
+#define SYNTI2_F_VS3 72
+#define SYNTI2_F_VS4 73
+#define SYNTI2_F_VSN 74
+#define SYNTI2_F_VSC 75
+#define SYNTI2_F_PF1 76
+#define SYNTI2_F_PF2 77
+#define SYNTI2_F_PF3 78
+#define SYNTI2_F_PF4 79
+#define SYNTI2_F_PFN 80
+
+#define SYNTI2_F_NPARS 128
+
+#endif

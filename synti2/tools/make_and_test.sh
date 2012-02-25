02cdd78700563ff5e1362edd958efeddc0ac22a1
Paavo Nieminen <paavo.j.nieminen@jyu.fi>
2012-02-25 13:40:05 +0200

    Renamed the jack client test more intelligently as jsynti2. Moved it to a tests directory.

--- synti2/tools/make_and_test.sh
+++ synti2/tools/make_and_test.sh
@@ -1,1 +1,1 @@
-make patchedit && make jacktest && killall testi ; killall patchedit ; ./testi & sleep 2 && ./patchedit & sleep 4 &&  aj-snapshot -r tools/s2devel.ajsnapshot 
+make patchedit && make jacktest && killall jsynti2 ; killall patchedit ; ./jsynti2 & sleep 2 && ./patchedit & sleep 4 &&  aj-snapshot -r tools/s2devel.ajsnapshot 

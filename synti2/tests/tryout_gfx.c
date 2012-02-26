245bb37df121a323c745065810f31717ffaa6c5f
Paavo Nieminen <paavo.j.nieminen@jyu.fi>
2012-02-27 00:35:32 +0200

    Some strange creatures I drew with OpenGL...

--- synti2/tests/tryout_gfx.c
+++ synti2/tests/tryout_gfx.c
@@ -8,7 +8,65 @@
 
 #include "synti2_guts.h"
 
-#define NSTRIPS 25
+static void primit(){
+      glBegin(GL_QUADS);
+      glVertex3i( -1,   -1,   -1);
+      glVertex3i(  1,   -1,   -1);
+      glVertex3i(  1,    1,   -1);
+      glVertex3i(  -1,    1,   -1);
+
+      glVertex3i(  -1,    1,   1);
+      glVertex3i(  1,    1,   1);
+      glVertex3i(  1,   -1,   1);
+      glVertex3i( -1,   -1,   1);
+
+      /*
+      glVertex3i(  1,  -1,   1);
+      glVertex3i(  1,  1,    1);
+      glVertex3i(  1,  1,   -1);
+      glVertex3i(  1, -1,   -1);
+
+      glVertex3i( -1, -1,   -1);
+      glVertex3i( -1,  1,   -1);
+      glVertex3i( -1,  1,    1);
+      glVertex3i( -1,  -1,   1);
+
+      glVertex3i(    -1, 1,  1);
+      glVertex3i(    1,  1,  1);
+      glVertex3i(    1,  1, -1);
+      glVertex3i(   -1,  1, -1);
+
+      glVertex3i(  -1, -1 , -1);
+      glVertex3i(  1, -1 , -1);
+      glVertex3i(  1, -1 ,  1);
+      glVertex3i(  -1, -1 , 1);
+      */
+
+      glEnd();
+}
+
+static void recurs(int n, float am){
+  if (n==0) return;
+  primit();
+  glRotatef(am, 1.f, am ,2.f*n);
+  glTranslatef(1.f,-1.f,0.f);
+  glScalef(.99f,.99f,.99f);
+  recurs(n/2,am);
+  recurs(n/2,-am*2);
+}
+
+static void morko(int par, float par2){
+  int j;
+    for(j=0; j<9; j++){
+      
+      glRotatef(40,0.,1.,2.);
+      glPushMatrix();
+      recurs(par, par2);
+      glPopMatrix();
+
+    }
+}
+
 
 /** Paint a triangle :). */
 static void render_scene(const synti2_synth *s){
@@ -22,40 +80,27 @@ static void render_scene(const synti2_synth *s){
   glClear(GL_DEPTH_BUFFER_BIT|GL_COLOR_BUFFER_BIT);
 
   glEnable(GL_BLEND);
+  glBlendFunc(GL_SRC_ALPHA,GL_ONE_MINUS_SRC_ALPHA);
 
   glLoadIdentity();
-  glTranslatef(0.,0.,-150 +100. * sin(time) - .1*s->note[0]);
+  glTranslatef(0.f, 0.f ,-160.f); // +100. * sin(time) - .1*s->note[0]);
   /*glTranslatef(data[400]*time,data[600]*time,-150);*/
-  glRotatef(time*3,0.,1.,0.);
-  glRotatef(time*7,0.,0.,1.);
-
-  glBlendFunc(GL_SRC_ALPHA,GL_ONE_MINUS_SRC_ALPHA);
   
+  glRotatef(time*13.f, 1.f, 0.f, 0.f);
+  glRotatef(time*20.f, 1.f, 1.f, 0.f);
+  glRotatef(time*11.f, 0.f, 0.f, 1.f);
+
+  
+  glTranslatef(-100.f, (i%3) * 10.f, 0.f);
   glPushMatrix();
-  for(j=0; j<NSTRIPS; j++){
-    glPopMatrix();
 
-    glRotatef(180.+22.5,0.,1.,0.);
-    glRotatef(j*15,1.,0.,0.);
+  for(i=0; i<10; i++){
+    glRotatef(45.f, 1.f, 0.f, 0.f);
+    glTranslatef(20.f, (i%3) * 10.f, 0.f);
 
-    glPushMatrix();
+    glColor4f(.5f+.5f*sin(time),s->eprog[3][1].f,.5f+.5f*sin(time+1),1.0f/i);
 
-    for(i=0; i<120; i++){
-
-      glTranslatef(1. /*+ sin(time)*/,0.,0.);
-      /*
-      glColor4f(.3+data[((i+0))%dlen], 
-                .3+data[((i+100))%dlen], 
-                .3+data[((i+200))%dlen], 8.0/i);
-      */
-
-      /* The primitive: a tri-angleee */
-      glBegin(GL_TRIANGLES);
-      glVertex3f( 0,   1, 0);
-      glVertex3f( 1,  -1, 0);
-      glVertex3f(-2,  -1, i/8);
-      glEnd();
-    }
+    morko(62+30*sin(time*i), time+i*2);
   }
   glPopMatrix();
 }

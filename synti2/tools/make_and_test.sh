f431e5762b85563718ff79372dc4c20fca0389ab
Paavo Nieminen <paavo.j.nieminen@jyu.fi>
2012-02-29 14:20:52 +0200

    The new 'outsourced' midi filter is now operational, although still a bubblegum hack.

--- synti2/tools/make_and_test.sh
+++ synti2/tools/make_and_test.sh
@@ -1,1 +1,3 @@
 make patchedit && make jsynti2 && killall jsynti2 ; killall patchedit ; ./jsynti2 & sleep 2 && ./patchedit & sleep 4 &&  aj-snapshot -r tools/s2devel.ajsnapshot 
+
+make patchedit && make jsynti2 && make jmiditrans && killall jsynti2 ; killall patchedit ; ./jsynti2 & sleep 2 && ./patchedit & sleep 3 &&  aj-snapshot -r tools/s2devel.ajsnapshot

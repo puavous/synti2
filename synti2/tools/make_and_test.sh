f7170ecec7518b76d86103568be284020c9edb38
Paavo Nieminen <paavo.j.nieminen@jyu.fi>
2012-03-07 23:24:06 +0200

    I just realized that the sequencer is useless, if music is generated programmatically.

--- synti2/tools/make_and_test.sh
+++ synti2/tools/make_and_test.sh
@@ -2,6 +2,10 @@
 
 make tinyexe MAIN=tests/tinyplayer.c SND=patches/minimal.s2bank SMF=tests/minimal.mid
 
+# Extremely small tiny test (with no sequencer even):
+
+make tinyexe MAIN=tests/tinyplayer.c SND=patches/minimal.s2bank SMF=tests/minimal.mid ADDFLAGS=-msse4 NONOS='-DNO_NOTHING -DDRYRUN -DEXTREME_NO_SEQUENCER'
+
 make patchedit && make jsynti2 && killall jsynti2 ; killall patchedit ; ./jsynti2 & sleep 2 && ./patchedit & sleep 4 &&  aj-snapshot -r tools/s2devel.ajsnapshot 
 
 make patchedit && make jsynti2 && make jmiditrans && killall jsynti2 ; killall patchedit ; ./jsynti2 & sleep 2 && ./patchedit & sleep 3 &&  aj-snapshot -r tools/s2devel.ajsnapshot

1336ae57fcb11afc0af76722aff62c6377004aaf
Paavo Nieminen <paavo.j.nieminen@jyu.fi>
2012-03-10 22:40:48 +0200

    This will be the final version of 'The Cube Thing' and associated files.

--- synti2/example/pikkune_gfx.c
+++ synti2/example/pikkune_gfx.c
@@ -37,7 +37,7 @@ static void himpale(int par1, float par2){
   int i;
   glPushMatrix();
   for(i=0;i<par1;i++){
-    glRotatef(360/par1, 0.f, 0.f, 1.f);
+    glRotatef(360.f/par1, 0.f, 0.f, 1.f);
     glPushMatrix();
     glTranslatef(par2, 0.f, 0.f);
     kuutio();
@@ -51,20 +51,24 @@ static void himpale(int par1, float par2){
 static void render_scene(const synti2_synth *s){
   int i, j;
 
-  float amb[4];
-  float spec[4];
-
   float time = (float)(s->framecount.val) / s->sr;
 
-   GLfloat mat_specular[] = { 1.0f, 1.0f, 1.0f, 1.0f };
-   GLfloat mat_diffuse[] = { 1.0f, 0.0f, 0.0f, 1.0f };
+  //   GLfloat mat_specular[] = { 1.0f, 1.0f, 1.0f, 1.0f };
+  //GLfloat mat_specular[] = { 1.0f, 1.0f, 1.0f, 1.0f };
+  GLfloat mat_ambient[4];
+  GLfloat mat_specular[4];
+  for(i=0;i<4;i++){
+    mat_specular[i] = 1.f - s->eprog[2][3].f;
+  }
+   
+   //   GLfloat mat_diffuse[] = { 1.0f, 0.0f, 0.0f, 1.0f };
    GLfloat mat_shininess[] = { 50.0f };
    GLfloat light_position[] = { 1.0f, 1.0f, 1.0f, 0.0f };
 
    float cf = s->eprog[1][1].f;
    glClearColor (cf, cf, cf, 0.0);
 
-   glShadeModel (GL_SMOOTH);
+   //   glShadeModel (GL_SMOOTH);  // Default?
 
    glMaterialfv(GL_FRONT, GL_SPECULAR, mat_specular);
    glMaterialfv(GL_FRONT, GL_DIFFUSE, mat_specular);
@@ -77,26 +81,26 @@ static void render_scene(const synti2_synth *s){
 
    glClear(GL_DEPTH_BUFFER_BIT|GL_COLOR_BUFFER_BIT);
 
-
   glLoadIdentity();
   glLightfv(GL_LIGHT0, GL_POSITION, light_position);
 
-  glTranslatef(0.f, 0.f ,-20.f); // +100. * sin(time) - .1*s->note[0]);
+  glTranslatef(0.f, 0.f , -20.f); // +100. * sin(time) - .1*s->note[0]);
   /*glTranslatef(data[400]*time,data[600]*time,-150);*/
+
+  // glScalef(mat_specular[0], mat_specular[0],mat_specular[0]);
+
 
   //glRotatef(time*45.f, 0.f, 1.f, 0.f);
   glRotatef(time*(1.f+s->note[4]), 1.f, s->note[4] % 3, 0.f);
-
-  //glScalef(1.f, 1.f, 1.f);
 
   int a = s->note[4] % 13;
 
   int n = 10 + (a);
   for(i = 0; i<n; i++){
-    glPushMatrix();
-    himpale(6, 10+cf);
-    glPopMatrix();
-    glTranslatef(0.f, 0.f, 3.f);
+    //    glPushMatrix();
+    himpale(6, 10.f+cf);
+    //    glPopMatrix();
+    glTranslatef(0.f, 0.f, 3.f-cf);
     glRotatef(360.f/n, 1.f, 0.f, 0.f);
   }
 }

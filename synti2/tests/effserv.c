f7cd6f578bec8d46fe121347d2df729ff953c8d5
Paavo Nieminen <paavo.j.nieminen@jyu.fi>
2012-03-11 21:30:54 +0200

    Fill a buffer of light commands, to be sent all at once.

--- synti2/tests/effserv.c
+++ synti2/tests/effserv.c
@@ -11,8 +11,39 @@
 #define PORT 9909
 #define SRV_IP "192.168.10.1"
 
+#define LEFFLEN 6
+
+
+/* hack for today. */
+void init_light_package(unsigned char * buf, int nlights){
+  int i;
+  memset(buf, 0, nlights*sizeof(unsigned char));
+  buf[0]=1; /* Spec version 1 */
+  buf++;
+  for(i=0;i<nlights;i++){
+    buf[i*LEFFLEN] = 1; /* effect type is light. */
+    buf[i*LEFFLEN+1] = i; /* light indices. */
+  }
+}
+
+/* Can set from same values as colors in the OpenGL visualization */
+void set_light_rgb_fv(unsigned char * buf, int lightind, float *rgb){
+  float f;
+  int i;
+  for(i=0;i<3;i++){
+    /*clamp*/
+    if (*rgb < 0.f) f = 0.f;
+    else if (*rgb > 1.f) f = 1.f;
+    else f = *rgb;
+    buf[1 + lightind * LEFFLEN + 3 + i] = (255.f * f);
+    rgb++;
+  }
+}
+
+
 const char light[] = {
   1, /* Speksin versio aina yksi*/
+
   1, /* Tehosteen tyyppi on yksi eli valo*/
   0, /* Ensimmäinen valo löytyy indeksistä nolla*/
   0, /* Valon tyyppi on nolla eli RGB*/
@@ -36,6 +67,10 @@ const char light[] = {
     0, 
     255, 
 };
+
+#define NLIGHTS 4
+char light_packet[1+LEFFLEN*NLIGHTS];
+
 
 void diep(char *s)
 {
@@ -67,9 +102,16 @@ int main(void)
     exit(1);
   }
 
-  int buflen = sizeof(light);
+  float my_lights[] = {1.f, 0.f, 0.f};
+  init_light_package(light_packet, NLIGHTS);
 
-  if (sendto(s, light, buflen, 0, &si_other, slen)==-1){
+  for(i=0; i<NLIGHTS; i++){
+    set_light_rgb_fv(light_packet, i, my_lights);
+  }
+
+  int buflen = sizeof(light_packet);
+
+  if (sendto(s, light_packet, buflen, 0, &si_other, slen)==-1){
 #ifndef ULTRASMALL
     diep("sendto()");
 #endif

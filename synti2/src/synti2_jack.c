1d1ede2b6395c125bf0d105462fe6a4f95ced879
Paavo Nieminen <paavo.j.nieminen@jyu.fi>
2012-03-26 12:52:09 +0300

    Probably broken at the moment; may not even compile.. I started to build the new MIDI-to-MISSS interface

--- synti2/src/synti2_jack.c
+++ synti2/src/synti2_jack.c
@@ -7,6 +7,101 @@
 /* The whole thing is relevant only if compiling for Jack. */
 #ifdef JACK_MIDI
 
+/* FIXME: Move most of the methods to a MIDI-to-MISSS translator
+   module, since jack is necessary only as the outer interface
+   layer. The same module can be used with whatever MIDI interface
+   (ALSA, VST..). Put the MIDI defines in the corresponding header
+   file. That could be a place also for synti2 channel allocation?
+   Maybe? But that needs a bigger revamp of the tool-engine
+   responsibility share.
+ */
+
+/* Some defines to make midi handling more clear in the code part: */
+#define MIDI_STATUS_NOTE_OFF 0x8;
+#define MIDI_STATUS_NOTE_ON 0x9;
+#define MIDI_STATUS_KEY_PRESSURE 0xa;
+#define MIDI_STATUS_CONTROL 0xb;
+#define MIDI_STATUS_PROGRAM 0xc;
+#define MIDI_STATUS_CHANNEL_PRESSURE 0xd;
+#define MIDI_STATUS_PITCH_WHEEL 0xe;
+#define MIDI_STATUS_SYSTEM 0xf;
+
+
+
+/** Creates a MISSS note message; returns length of the
+ *  message. Output buffer must have enough space. Currently, the
+ *  length is 4.
+ */
+static
+int
+synti2_misss_note(byte_t *misss_out, 
+                  byte_t misss_chn, 
+                  byte_t midi_note,
+                  byte_t midi_vel){
+  *misss_out++ = MISSS_MSG_NOTE;
+  *misss_out++ = misss_chn; /* TODO: Channel dispersion logic in caller?*/
+  *misss_out++ = midi_note;
+  *misss_out++ = midi_vel;
+  return 4;
+}
+
+/**
+ * Converts a MIDI message (complete; no running status) into a MISSS
+ * message that the synti2 synthesizer can handle; returns the length
+ * of the output MISSS message. There must be enough space in the
+ * output buffer (TODO: document the maximum message size increase).
+ *
+ * FIXME: Move this to a non-jack specific MIDI interpreter module.
+ *
+ * FIXME: Need a MIDI filter module in C, with parameters, after all?
+ * If the whole synti2 interface is intended to be in one programming
+ * language(?) probably so.. so do it, pls.
+ */
+static
+int
+synti2_midi_to_misss(byte_t *midi_status, 
+                     byte_t *misss_out, 
+                     int input_size)
+{
+  int midi_status;
+  int midi_chn;
+  int midi_vel = 0;
+  int midi_note = 0;
+  midi_status = *midi_in++;
+  midi_chn = midi_status & 0x0f;
+
+  switch(midi_status >> 4){
+  case MIDI_STATUS_NOTE_OFF:
+    return synti2_misss_note(misss_out, midi_chn, *midi_in, 0);
+  case MIDI_STATUS_NOTE_ON:
+    midi_note = *midi_in++;
+    midi_vel = *midi_in++;
+    return synti2_misss_note(misss_out, midi_chn, midi_note, midi_vel);
+  case MIDI_STATUS_KEY_PRESSURE 0xa:
+    /* Key pressure becomes channel pressure (no polyphonic pressure). */
+    /* return synti2_misss_control(misss_out, midi_chn, 
+       synti2_misss_mapPressureDest(), synti2_misss_mapPressureValue());*/
+  case MIDI_STATUS_CONTROL 0xb:
+    /* return synti2_misss_control(misss_out, midi_chn, 
+       synti2_misss_mapControlDest(xx), 
+       synti2_misss_mapControlValue(yy));*/
+  case MIDI_STATUS_PROGRAM 0xc:
+    /* Omit program change. Could have some sound bank logic... */
+    return 0;
+  case MIDI_STATUS_CHANNEL_PRESSURE 0xd:
+    /* return synti2_misss_control(misss_out, midi_chn, 
+       synti2_misss_mapPressureDest(), synti2_misss_mapPressureValue());*/
+  case MIDI_STATUS_PITCH_WHEEL 0xe:
+    /* return synti2_misss_control(misss_out, midi_chn, 
+       synti2_misss_mapPitchDest(), synti2_misss_mapPitchValue());*/
+  case MIDI_STATUS_SYSTEM 0xf:
+    /* return synti2_misss_data(xx,yy,zz)*/
+  default:
+  }
+  return 0;
+}
+
+
 /** Creates a future for the player object that will repeat the next
  *  nframes of midi data from a jack audio connection kit midi port.
  *
@@ -24,6 +119,7 @@ synti2_player_init_from_jack_midi(synti2_player *pl,
   jack_nframes_t i, nev;
   byte_t *msg;
   void *midi_in_buffer  = (void *) jack_port_get_buffer (inmidi_port, nframes);
+  int out_size;
 
   /* Re-initialize, and overwrite any former data. */
   pl->playloc = pl->evpool; /* This would "rewind" the song */
@@ -38,13 +134,15 @@ synti2_player_init_from_jack_midi(synti2_player *pl,
 
       /* Get next available spot from the data pool */
       msg = pl->data + pl->idata;
-      memcpy(msg,ev.buffer,ev.size);  /* deep copy here */
-      pl->idata += ev.size; /*Update the data pool top*/
+
+      out_size = synti2_midi_to_misss(ev.buffer, msg, ev.size);
+
+      pl->idata += out_size; /*Update the data pool top*/
 
       synti2_player_event_add(pl, 
                               pl->frames_done + ev.time, 
                               msg, 
-                              ev.size);
+                              out_size);
     } else {
       break;
     }

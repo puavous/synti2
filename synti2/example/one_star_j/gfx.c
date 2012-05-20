97da81f1632c44b148448d513c004a1314f1a4ac
Paavo Nieminen <paavo.j.nieminen@jyu.fi>
2012-05-20 23:30:56 +0300

    Added files for the unfinished Stream entry-to-be which never met the deadline.

--- synti2/example/one_star_j/gfx.c
+++ synti2/example/one_star_j/gfx.c
@@ -10,7 +10,8 @@
 
 static void face(){
   static float data[] = {
-    -0.90451f,   0.29390f,   0.58779f
+    -0.36180f,   0.11756f,   0.58779f
+    //-0.90451f,   0.29390f,   0.58779f
   };
 
   glBegin(GL_TRIANGLES);
@@ -24,26 +25,26 @@ static void face(){
 
   //  glNormal3f(-0.90451f,   0.29390f,   0.58779f);
   glNormal3fv(data);
-  glVertex3f(  0.f,  0.f,    .5f);
+  glVertex3f(  0.f,  0.f,    .2f);
   glEnd();
 }
 
 static void star(){
-  //  static float s = 360.f/5;
+  static float s = 360.f/5;
   glPushMatrix();
   face();
-  glRotatef(360.f/5, 0.f, 0.f, 1.f); face();
-  glRotatef(360.f/5, 0.f, 0.f, 1.f); face();
-  glRotatef(360.f/5, 0.f, 0.f, 1.f); face();
-  glRotatef(360.f/5, 0.f, 0.f, 1.f); face();
+  glRotatef(s, 0.f, 0.f, 1.f); face();
+  glRotatef(s, 0.f, 0.f, 1.f); face();
+  glRotatef(s, 0.f, 0.f, 1.f); face();
+  glRotatef(s, 0.f, 0.f, 1.f); face();
 
   glRotatef(180.f, 0.f, 1.f, 0.f);
 
   face();
-  glRotatef(360.f/5, 0.f, 0.f, 1.f); face();
-  glRotatef(360.f/5, 0.f, 0.f, 1.f); face();
-  glRotatef(360.f/5, 0.f, 0.f, 1.f); face();
-  glRotatef(360.f/5, 0.f, 0.f, 1.f); face();
+  glRotatef(s, 0.f, 0.f, 1.f); face();
+  glRotatef(s, 0.f, 0.f, 1.f); face();
+  glRotatef(s, 0.f, 0.f, 1.f); face();
+  glRotatef(s, 0.f, 0.f, 1.f); face();
 
 
   glPopMatrix();
@@ -81,52 +82,59 @@ static void render_scene(const synti2_synth *s){
   */
    
    //   GLfloat mat_diffuse[] = { 1.0f, 0.0f, 0.0f, 1.0f };
-   GLfloat mat_shininess[] = { 50.0f };
-   GLfloat light_position[] = { 1.0f, 1.0f, 1.0f, 0.0f };
+  //   GLfloat mat_shininess[] = { 50.0f };
+  //   GLfloat light_position[] = { 1.0f, 1.0f, 1.0f, 0.0f };
 
-   float cf = s->eprog[1][1].f;
-   glClearColor (cf, cf, cf, 0.0);
+  //   float cf = s->eprog[1][1].f;
+  //   glClearColor (cf, cf, cf, 0.0);
 
    //   glShadeModel (GL_SMOOTH);  // Default?
 
-   glMaterialfv(GL_FRONT, GL_SPECULAR, mat_specular);
    glMaterialfv(GL_FRONT, GL_DIFFUSE, mat_specular);
+   glMaterialfv(GL_FRONT, GL_SPECULAR, mat_specular);
    glMaterialfv(GL_FRONT, GL_AMBIENT, mat_specular);
-   glMaterialfv(GL_FRONT, GL_SHININESS, mat_shininess);
-   glLightfv(GL_LIGHT0, GL_POSITION, light_position);
+   //glMaterialfv(GL_FRONT, GL_SHININESS, mat_shininess);
+   //   glLightfv(GL_LIGHT0, GL_POSITION, light_position);
 
    glEnable(GL_LIGHTING);
    glEnable(GL_LIGHT0);
    glEnable(GL_DEPTH_TEST);
-   glEnable(GL_CULL_FACE);
+   //   glEnable(GL_CULL_FACE);
 
    glClear(GL_DEPTH_BUFFER_BIT|GL_COLOR_BUFFER_BIT);
 
-  glLoadIdentity();
-  glLightfv(GL_LIGHT0, GL_POSITION, light_position);
+   glLoadIdentity();
+   //  glLightfv(GL_LIGHT0, GL_POSITION, light_position);
 
-  glTranslatef(0.f, 0.f , -20.f); // +100. * sin(time) - .1*s->note[0]);
-  glRotatef(90.f, 0.f, 0.f , 1.f); // +100. * sin(time) - .1*s->note[0]);
+   glTranslatef(0.f, 0.f , -2.f); // +100. * sin(time) - .1*s->note[0]);
+   //  glRotatef(90.f, 0.f, 0.f , 1.f); // +100. * sin(time) - .1*s->note[0]);
 
-  glRotatef(time*(1.f+s->note[4]), 1.f, s->note[4] % 3, 0.f);
+   //glRotatef(time*4.f + time*(1.f+s->note[4]), 1.f, s->note[4] % 3, 0.f);
+   glRotatef(time*16.f, 0.f, 1.f, 0.f);
+   
+   //  int a = s->note[4] % 13;
 
-  int a = s->note[4] % 13;
-
-  int n = 10 + (a);
+  star();
+  //    himpale(6, 14.f+cf);
+    /*
+  //  int n = 10 + (a);
+  int n = 2;
   for(i = 0; i<n; i++){
     //    glPushMatrix();
     himpale(6, 14.f+cf);
     //    glPopMatrix();
     glTranslatef(0.f, 0.f, 3.f-cf);
-    glRotatef(360.f/n, 1.f, 0.f, 0.f);
+    //    glRotatef(360.f/n, 1.f, 0.f, 0.f);
   }
+    */
 }
 
 /** Render something that varies with time and "audio snapshot". */
 void render_using_synti2(const synti2_synth *s){
   glMatrixMode(GL_PROJECTION);
   glLoadIdentity();
-  glFrustum(-1.33,1.33,-1,1,1.5,400);
+  glFrustum(-1.33f,1.33f,-1.f,1.f,1.f,400.f);
+
 
   glMatrixMode(GL_MODELVIEW);
   glEnable(GL_DEPTH_TEST);

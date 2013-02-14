b4561bbb57dd889944de0268277aa29c7f0c0824
Paavo Nieminen <paavo.j.nieminen@jyu.fi>
2013-02-14 22:29:13 +0200

    Clarified some definitions. The spec is beginning to form.

--- synti2/include/synti2_midi.h
+++ synti2/include/synti2_midi.h
@@ -2,7 +2,7 @@
 #define SYNTI2_MIDI_TRANSLATOR_H
 
 /* Some defines to make midi handling more clear in the code part.
- * These come directly from the MIDI standard: 
+ * These come directly from the MIDI standard:
  */
 #define MIDI_STATUS_NOTE_OFF 0x8
 #define MIDI_STATUS_NOTE_ON 0x9
@@ -15,28 +15,52 @@
 
 #define MIDI_STATUS_SYSTEM_META 0xf
 
+/* My own SysEx opcodes. Could name these SysEx or Syx, but I think
+ * SEx sounds sexier.
+ * 
+ * Yep, and these need to be in yet another separate header..
+ */
+#define SYNTI2_SEX_PART_RESET     0
+#define SYNTI2_SEX_PART_MODE      1
+#define SYNTI2_SEX_PART_CHANLIST  2
+#define SYNTI2_SEX_PART_CHANMAP   3
+#define SYNTI2_SEX_PART_CONTROL   4
+#define SYNTI2_SEX_PART_PITCHDEST 5
+#define SYNTI2_SEX_PART_RECVNOFF  6
+#define SYNTI2_SEX_PART_VELOCITY  7
+#define SYNTI2_SEX_PART_INSTARAMP 8
+
+
+#define SYNTI2_PART_MODE_UNISON      0
+#define SYNTI2_PART_MODE_POLY_ROTATE 1
+#define SYNTI2_PART_MODE_POLY_STACK  2
+#define SYNTI2_PART_MODE_MAPPED      3
+#define SYNTI2_PART_MODE_MUTE        4
+
+
 #include "synti2.h"
 #include "synti2_guts.h"
 
+/** This structure is used for controlling the midi translator
+ *  module. The sound engine is totally unaware of this.
+ */
 typedef struct {
-  int output_base_channel;    /* can shift channels +/- */
-  int slave_channels;         /* polyphony */
-  int slave_channel_alg;      /* algorithm for poly allocate. 0=rotate; */
   int use_sustain_pedal;      /* TODO: not yet implemented; maybe never?. */
-  int block_note_off;         /* 0=use note off; other=don't use n-off. */
-  int constant_velocity;      /* 0=free; 1-127 force constant velocity. */
-  int bend_destination;       /* Controller to use for pitch bend. */
-  int cc_destination[128];    /* 128 controllers (MIDI) to internal controls. */
-  int note_channel_shift[128];/* For drum sounds and/or keyboard splits. */
-  float cc_min[NCONTROLLERS]; /* destination-specific min value. */
-  float cc_max[NCONTROLLERS]; /* destination-specific max value. */
-  float instant_ramp_length;  /* length of "intantaneous" transition */
+  int receive_note_off;       /* 0=don't use note off; other=receive n-off. */
+  int use_const_velocity;     /* 0=free; 1-127 force constant velocity. */
+  int channels[NPARTS];       /* unison duplicates / polyphony (1..#voices)*/
+  int note_channel_map[128];  /* For drum sounds and/or keyboard splits. */
+  int bend_destination;       /* Controller to use for pitch bend. 0=none? */
+  int mod_src[NCONTROLLERS];  /* 128 controllers (MIDI) to internal controls. */
+  float mod_min[NCONTROLLERS]; /* destination-specific min value. */
+  float mod_max[NCONTROLLERS]; /* destination-specific max value. */
+  float instant_ramp_length;   /* length of "intantaneous" transition */
+/*int cc_destination[128];*/  /* 128 controllers (MIDI) to internal controls. */
 } synti2_midi_channel_map;
 
 typedef struct {
   synti2_midi_channel_map chn[NPARTS];
 } synti2_midi_map;
-
 
 int
 synti2_midi_to_misss(byte_t *midi_in, 

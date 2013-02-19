f89116cffc3a0629dd8340ed3796e110e37e27a8
Paavo Nieminen <paavo.j.nieminen@jyu.fi>
2013-02-19 11:48:27 +0200

    Declarations for all necessary access methods; no implementations yet.

--- synti2/include/synti2_midi_guts.h
+++ synti2/include/synti2_midi_guts.h
@@ -15,11 +15,14 @@ typedef struct {
   int voices[NPARTS+1];       /* unison duplicates / polyphony (1..#voices)*/
   int note_channel_map[128];  /* For drum sounds and/or keyboard splits. */
   int bend_destination;       /* Controller to use for pitch bend. 0=none? */
+  int pressure_destination;   /* Controller to use for Channel Aftertouch. 0=none? */
   int mod_src[NCONTROLLERS];  /* 128 controllers (MIDI) to internal controls. */
   float mod_min[NCONTROLLERS]; /* destination-specific min value. */
   float mod_max[NCONTROLLERS]; /* destination-specific max value. */
-  float instant_ramp_length;   /* length of "intantaneous" transition */
-/*int cc_destination[128];*/  /* 128 controllers (MIDI) to internal controls. */
+  /* Internal: */
+  float instant_ramp_length;   /* length of "intantaneous" transition (expose?)*/
+/*int cc_destination[128];*/  /* Fast look-up helper table for the 128 MIDI
+                                 controllers to internal modulators.(needed?) */
 } synti2_midi_channel_map;
 
 typedef struct {
--- synti2/tools/midimaptool.hpp
+++ synti2/tools/midimaptool.hpp
@@ -19,17 +19,47 @@ namespace synti2{
     void write(std::ostream &os){ std::cerr << "FIXME: map write not implemented"; return; }
     void read(std::istream &ins){ std::cerr << "FIXME: map read not implemented"; return; }
     
-    void setNoff(int midichn, bool val);
-    bool getNoff(int midichn);
-    std::vector<unsigned char> sysexNoff(int midichn);
-    
     void setMode(int midichn, int val);
     int getMode(int midichn);
     std::vector<unsigned char> sysexMode(int midichn);
     
+    void setSust(int midichn, bool val);
+    bool getSust(int midichn);
+    std::vector<unsigned char> sysexSust(int midichn);
+
+    void setNoff(int midichn, bool val);
+    bool getNoff(int midichn);
+    std::vector<unsigned char> sysexNoff(int midichn);
+    
+    void setFixedVelo(int midichn, int val);
+    int getFixedVelo(int midichn);
+    std::vector<unsigned char> sysexFixedVelo(int midichn);
+
     void setVoices(int midichn, const std::string &val);
     std::string getVoicesString(int midichn);
     std::vector<unsigned char> sysexVoices(int midichn);
+
+    /* FIXME: Channel map here. */
+
+    void setBendDest(int midichn, int val);
+    int getBendDest(int midichn);
+    std::vector<unsigned char> sysexBendDest(int midichn);
+
+    void setPressureDest(int midichn, int val);
+    int getPressureDest(int midichn);
+    std::vector<unsigned char> sysexPressureDest(int midichn);
+
+    void setModSource(int midichn, int val);
+    int getModSource(int midichn);
+    std::vector<unsigned char> sysexModSource(int midichn);
+
+    void setModMin(int midichn, float val);
+    float getModMin(int midichn);
+    std::vector<unsigned char> sysexModMin(int midichn);
+
+    void setModMax(int midichn, float val);
+    float getModMax(int midichn);
+    std::vector<unsigned char> sysexModMax(int midichn);
     
   };
   

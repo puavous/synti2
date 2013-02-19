213e3d3b4252a05b8f7c6a0dae74ba76251c0e07
Paavo Nieminen <paavo.j.nieminen@jyu.fi>
2013-02-19 12:15:40 +0200

    Accessors created, not tested or hooked to GUI yet.

--- synti2/tools/midimaptool.cxx
+++ synti2/tools/midimaptool.cxx
@@ -4,47 +4,71 @@
 
 #include <sstream>
 
-void
-synti2::MidiMap::setNoff(int midichn, bool val){
-  mmap.chn[midichn].receive_note_off = val?1:0;
-}
-
-bool
-synti2::MidiMap::getNoff(int midichn){
-  return (mmap.chn[midichn].receive_note_off != 0);
-}
-
-std::vector<unsigned char>
-synti2::MidiMap::sysexNoff(int midichn){
+static
+std::vector<unsigned char> 
+sysexOneByte(int type, int midichn, int thebyte){
   std::vector<unsigned char> res;
   synti2_sysex_header(res);
-  res.push_back(MISSS_SYSEX_MM_NOFF);
+  res.push_back(type);
   res.push_back(midichn);
-  res.push_back(getNoff(midichn)?1:0);
+  res.push_back(thebyte);
   synti2_sysex_footer(res);
   return res;
 }
 
 void
 synti2::MidiMap::setMode(int midichn, int val){
-  mmap.chn[midichn].mode = val;
-}
+  mmap.chn[midichn].mode = val;}
 
 int
 synti2::MidiMap::getMode(int midichn){
-  return mmap.chn[midichn].mode;
-}
+  return mmap.chn[midichn].mode;}
 
 std::vector<unsigned char>
 synti2::MidiMap::sysexMode(int midichn){
-  std::vector<unsigned char> res;
-  synti2_sysex_header(res);
-  res.push_back(MISSS_SYSEX_MM_MODE);
-  res.push_back(midichn);
-  res.push_back(getMode(midichn));
-  synti2_sysex_footer(res);
-  return res;
-}
+  return sysexOneByte(MISSS_SYSEX_MM_MODE,midichn,getMode(midichn));}
+
+
+void
+synti2::MidiMap::setSust(int midichn, bool val){
+  mmap.chn[midichn].use_sustain_pedal = val?1:0;}
+
+bool
+synti2::MidiMap::getSust(int midichn){
+  return (mmap.chn[midichn].use_sustain_pedal != 0);}
+
+std::vector<unsigned char>
+synti2::MidiMap::sysexSust(int midichn){
+  return sysexOneByte(MISSS_SYSEX_MM_SUST, midichn, 
+                      getSust(midichn)?1:0);}
+
+void
+synti2::MidiMap::setNoff(int midichn, bool val){
+  mmap.chn[midichn].receive_note_off = val?1:0;}
+
+bool
+synti2::MidiMap::getNoff(int midichn){
+  return (mmap.chn[midichn].receive_note_off != 0);}
+
+std::vector<unsigned char>
+synti2::MidiMap::sysexNoff(int midichn){
+  return sysexOneByte(MISSS_SYSEX_MM_NOFF, midichn, 
+                      getNoff(midichn)?1:0);}
+
+
+void
+synti2::MidiMap::setFixedVelo(int midichn, int val){
+  mmap.chn[midichn].use_const_velocity = val;}
+
+int
+synti2::MidiMap::getFixedVelo(int midichn){
+  return mmap.chn[midichn].use_const_velocity;}
+
+std::vector<unsigned char>
+synti2::MidiMap::sysexFixedVelo(int midichn){
+  return sysexOneByte(MISSS_SYSEX_MM_CVEL,midichn,getFixedVelo(midichn));}
+
+
 
 void 
 synti2::MidiMap::setVoices(int midichn, const std::string &val){
@@ -93,3 +117,36 @@ synti2::MidiMap::sysexVoices(int midichn){
   synti2_sysex_footer(res);
   return res;
 }
+
+/* FIXME: Channel map here */
+
+
+void
+synti2::MidiMap::setBendDest(int midichn, int val){
+  mmap.chn[midichn].bend_destination = val;}
+
+int
+synti2::MidiMap::getBendDest(int midichn){
+  return mmap.chn[midichn].bend_destination;}
+
+std::vector<unsigned char>
+synti2::MidiMap::sysexBendDest(int midichn){
+  return sysexOneByte(MISSS_SYSEX_MM_BEND,midichn,getBendDest(midichn));}
+
+
+void
+synti2::MidiMap::setPressureDest(int midichn, int val){
+  mmap.chn[midichn].pressure_destination = val;}
+
+int
+synti2::MidiMap::getPressureDest(int midichn){
+  return mmap.chn[midichn].pressure_destination;}
+
+std::vector<unsigned char>
+synti2::MidiMap::sysexPressureDest(int midichn){
+  return sysexOneByte(MISSS_SYSEX_MM_PRESSURE,midichn,getPressureDest(midichn));}
+
+
+
+/* FIXME: Mod sources, mins and maxes here. */
+

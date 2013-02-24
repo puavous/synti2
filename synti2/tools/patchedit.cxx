dae8bb97a0c2e175f96a531b6d1054158c8d824b
Paavo Nieminen <paavo.j.nieminen@jyu.fi>
2013-02-24 19:52:03 +0200

    I had broken it. Now I have fixed it. Should sleep more..

--- synti2/tools/patchedit.cxx
+++ synti2/tools/patchedit.cxx
@@ -308,12 +308,14 @@ void send_to_jack_process(const std::vector<unsigned char> &bytes){
   char sysex_buf[2000];
   size_t len = bytes.size();
 
-  /*
   for (int i = 0; i<bytes.size(); i++){
     sysex_buf[i] = bytes.at(i);
-    printf("0x%02x ", (int) ((unsigned char*)sysex_buf)[i]);
   }
-  printf("\n"); fflush(stdout);
+  /*
+  for (int i = 0; i<bytes.size(); i++){
+    printf("0x%02x ", (int) ((unsigned char*)sysex_buf)[i]);
+    printf("\n"); fflush(stdout);
+  }
   */
 
   size_t nwrit = jack_ringbuffer_write (global_rb, (char*)(&len), sizeof(size_t));

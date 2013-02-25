8dc6a4642855ed7ad2b07d01333813279cc925f9
Paavo Nieminen <paavo.j.nieminen@jyu.fi>
2013-02-25 23:39:16 +0200

    Decimation must be a parameter..

--- synti2/tools/midi2synti2.cxx
+++ synti2/tools/midi2synti2.cxx
@@ -47,7 +47,7 @@ int cmd_line_only(int argc, char **argv){
   ifs.close();
 
   /* FIXME: This needs to be made into a parameter very soon: */
-  ms.decimateTime(24);
+  /*ms.decimateTime(24);*/
 
   MidiEventTranslator tr;
 

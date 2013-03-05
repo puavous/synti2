22269efc2cb4ec0b423eece0e978faf8bbf1c1d6
Paavo Nieminen <paavo.j.nieminen@jyu.fi>
2013-03-05 22:02:04 +0200

    Rid of cbank, genpar, and midi2synti2

--- synti2/tools/make_and_test.sh
+++
@@ -1,9 +0,0 @@
-# quick tiny test:
-
-make tinyexe MAIN=tests/tinyplayer.c SND=patches/minimal.s2bank SMF=tests/minimal.mid
-
-# Extremely small tiny test (with no sequencer even):
-make tinyexe MAIN=tests/withoutseq.c SND=patches/miniFMset.s2bank SMF=tests/minimal.mid ADDFLAGS=-msse4 NONOS='-DNO_NOTHING -DEXTREME_NO_SEQUENCER'
-
-
-make tinyexe MAIN=tests/sdltest.c SND=example/example1.s2bank SMF=example/example1.mid GFX=tests/tryout_gfx2.c NONOS=''

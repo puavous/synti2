f6d27e7deb46c51da63a7f44962d9ff2a6948ad4
Paavo Nieminen <paavo.j.nieminen@jyu.fi>
2013-03-09 21:52:44 +0200

    Placeholders for feature management code.

---
+++ synti2/tools/featurehelper.cxx
@@ -0,0 +1,9 @@
+#include "featurehelper.hpp"
+
+#include <iostream>
+
+void
+synti2::Features::writeToStream(std::ostream &ost){
+  ost << "FIXME: Can't do yet" << std::endl;
+}
+/* TBD */
---
+++ synti2/tools/featurehelper.hpp
@@ -0,0 +1,19 @@
+#ifndef FEATURE_HELPER_HPP
+#define FEATURE_HELPER_HPP
+/** Manment of per-build synth features (part of capacities). Features
+ *  can be turned on or off, and only the selected ones will be
+ *  compiled in a synth executable.
+ */
+#include <string>
+#include <iostream>
+namespace synti2 {
+  class Features {
+    Features(std::string){};
+    bool hasFeature(std::string){
+      std::cerr << "FIXME: implement" << std::endl; return true;}
+    void readFromStream(std::istream &ist){};
+    void writeToStream(std::ostream &ost);
+  };
+}
+
+#endif

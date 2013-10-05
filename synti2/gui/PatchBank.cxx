300914ec4fa0178a72fd0e6f0f7c4e2d71703009
Paavo Nieminen <paavo.j.nieminen@jyu.fi>
2013-10-06 01:37:02 +0300

    Refactored a bit.. features and capacities are now encapsulated as classes.

--- synti2/gui/PatchBank.cxx
+++ synti2/gui/PatchBank.cxx
@@ -1,23 +1,113 @@
 #include "PatchBank.hpp"
+#include "Synti2Base.hpp"
 
 #include <iostream>
-PatchBank::PatchBank():patches(14)
-{
-  initFeatureDescriptions();
-  initCapacityDescriptions();
-  //toStream(std::cout); /*FIXME: for debug only.*/
-};
 
-void PatchBank::toStream(std::ostream & ost){
-  ost << "# Output by PatchBank::toStream()" << std::endl;
-  // FIXME: capacities.toStream(ostream);
-  // FIXME: features.toStream(ostream);
-  std::vector<Patch>::iterator pit;
-  for(pit=patches.begin(); pit!=patches.end(); ++pit){
-    (*pit).toStream(ost);
-  }
+namespace synti2base {
+
+void Capacities::addCapacityDescription(string key, 
+                                        string cname,
+                                        string humanReadable,
+                                        int min,
+                                        int max,
+                                        int initial,
+                                        string requiresf
+                                        )
+{
+  capkeys.push_back(key);
+  capValue[key] = initial;
+  caps.push_back(CapacityDescription(key,cname,humanReadable,min,max,requiresf));
 }
 
-void PatchBank::fromStream(std::istream & ist){
-  std::cerr << "PatchBank::fromStream() Can't read yet!" << std::endl;
+/** Initializes integer-type capacities of the synth. */
+  void Capacities::initCapacityDescriptions(){
+    addCapacityDescription("num_channels","NUM_CHANNELS","Number of channels (patches)",1,256,16,"");
+    addCapacityDescription("num_delays","NUM_DELAY_LINES","Number of delay lines",1,8,4,"delay");
+    addCapacityDescription("num_ops","NUM_ÖPERATORS","Number of operators/oscillators per channel", 0,4,4,"fm|add");
+    addCapacityDescription("num_envs", "NUM_ENVS", "Number of envelopes per channel", 1,6,2,"");
+    addCapacityDescription("num_knees","NUM_ENV_KNEES","Number of knees per envelope", 2,5,5,"");
+    addCapacityDescription("num_mods","NUM_MODULATORS","Number of controllable modulators per channel",0,4,4,"mods");
+  }
+
+  void Capacities::toStream(std::ostream &ost){
+    ost << "# Can't really output capacities yet. But the code is here." << std::endl;
+  }
+
+  
+  void Features::addFeatureDescription(string key, 
+                                       string cname,
+                                       string humanReadable,
+                                       string requires)
+  {
+    featkeys.push_back(key);
+    featureEnabled[key] = false; // init everything to disabled.
+    feats.push_back(FeatureDescription(key, cname, humanReadable, requires));
+  }
+  
+  /** Initializes the on/off -toggled feature descriptions */
+  void Features::initFeatureDescriptions(){
+    /*
+      These to be toggled by "COMPOSE_MODE" switch only:
+      
+      addFeatureDescription("sysex","FEAT_COMPOSE_MODE_SYSEX","Receive SysEx (usable in compose mode only)",""); // Need this at all?
+      addFeatureDescription("troubleshooting","FEAT_SAFETY","Safe mode (usable in compose mode only)","");
+    */
+    addFeatureDescription("fm","FEAT_APPLY_FM","Use frequency modulation","");
+    addFeatureDescription("add","FEAT_APPLY_ADD","Use addition matrix","");
+    addFeatureDescription("noise","FEAT_NOISE_SOURCE","Use noise source","");
+    addFeatureDescription("pdetune","FEAT_PITCH_DETUNE","Enable detuning of operators","");
+    /*
+      This won't be necessary in the final version. 1/10 cents is
+      enough accuracy for everybody. (or is it!? experience will tell.)
+      
+      addFeatureDescription("pfine","FEAT_PITCH_FINE_DETUNE","Enable fine detune (convenience only!)","");
+    */
+    addFeatureDescription("delay","FEAT_DELAY_LINES","Enable delay effects","");
+    addFeatureDescription("squash","FEAT_OUTPUT_SQUASH","Squash function at output","");
+    addFeatureDescription("phase","FEAT_RESET_PHASE","Enable phase reset on note-on","");
+    addFeatureDescription("waves","FEAT_EXTRA_WAVETABLES","Generate extra wavetables with harmonics","");
+    addFeatureDescription("cube","FEAT_POWER_WAVES","Enable squared/cubed waveforms","");
+    addFeatureDescription("mods","FEAT_MODULATORS","Use modulators / parameter ramps","");
+    addFeatureDescription("pbend","FEAT_PITCH_BEND","Enable the 'pitch bend' modulator","mods");
+    addFeatureDescription("noff","FEAT_NOTE_OFF","Listen to note off messages","");
+    addFeatureDescription("velocity","FEAT_VELOCITY_SENSITIVITY","Respond to velocity","");
+    addFeatureDescription("looping","FEAT_LOOPING_ENVELOPES","Enable looping envelopes","");
+    addFeatureDescription("legato","FEAT_LEGATO","Enable legato","");
+    addFeatureDescription("pscale","FEAT_PITCH_SCALING","Enable pitch scaling","");
+    addFeatureDescription("filter","FEAT_FILTER","Enable the lo/band/hi-pass filter","");
+    addFeatureDescription("fres","FEAT_FILTER_RESO_ADJUSTABLE","Enable adjustable filter resonance","");
+    addFeatureDescription("fresenv","FEAT_FILTER_RESO_ENVELOPE","Enable resonance envelope","");
+    addFeatureDescription("fcutenv","FEAT_FILTER_CUTOFF_ENVELOPE","Enable cutoff envelope","");
+    addFeatureDescription("fpfollow","FEAT_FILTER_FOLLOW_PITCH","Enable filter frequency to follow pitch","");
+    addFeatureDescription("fnotch","FEAT_FILTER_OUTPUT_NOTCH","Enable notch filter","");
+    addFeatureDescription("csquash","FEAT_CHANNEL_SQUASH","Enable per-channel squash function","");
+    addFeatureDescription("stereo","FEAT_STEREO","Enable stereo output","");
+    addFeatureDescription("panenv","FEAT_PAN_ENVELOPE","Enable pan envelope","stereo");
+  }
+ 
+  void Features::toStream(std::ostream &ost){
+    ost << "# Can't really output features yet. But the code is here." << std::endl;
+  }
+ 
+  
+  
+  PatchBank::PatchBank():patches(14)
+  {
+    toStream(std::cout); /*FIXME: for debug only.*/
+  };
+  
+  void PatchBank::toStream(std::ostream & ost){
+    ost << "# Output by PatchBank::toStream()" << std::endl;
+    caps.toStream(ost);
+    feats.toStream(ost);
+    std::vector<Patch>::iterator pit;
+    for(pit=patches.begin(); pit!=patches.end(); ++pit){
+      (*pit).toStream(ost);
+    }
+  }
+  
+  void PatchBank::fromStream(std::istream & ist){
+    std::cerr << "PatchBank::fromStream() Can't read yet!" << std::endl;
+  }
+  
 }
--- synti2/gui/PatchBank.hpp
+++ synti2/gui/PatchBank.hpp
@@ -17,6 +17,64 @@ using std::istream;
 
 namespace synti2base {
 
+  /* The following things need quite a close binding, because there
+     are rules connecting capacities, features, and parameters that
+     need/not to be set in a patch. */
+
+  /** "Capacities" of a synti2 stand-alone compilation - integral
+   * numbers of channels and oscillators..
+   */
+  class Capacities {
+  private:
+    std::vector<string> capkeys;
+    std::vector<CapacityDescription> caps;
+    std::map<string,int> capValue;
+    void addCapacityDescription(string key, 
+                                string cname,
+                                string humanReadable,
+                                int min,
+                                int max,
+                                int initial,
+                                string requiresf
+                                );
+    /** Initializes integer-type capacities of the synth. */
+    void initCapacityDescriptions();
+  public:
+    Capacities(){initCapacityDescriptions();}
+    std::vector<CapacityDescription>::iterator
+    begin(){return caps.begin();}
+    std::vector<CapacityDescription>::iterator
+    end(){return caps.end();}
+    int value(std::string key){
+      return capValue[key];
+    }
+    void toStream(std::ostream &ost);
+  };
+
+  /** "Features" of a synti2 stand-alone compilation - boolean on/off
+   * values of synth modules to be compiled and to take part in
+   * compilation.
+   */
+  class Features {
+  private:
+    std::vector<string> featkeys;
+    std::vector<FeatureDescription> feats;
+    std::map<string,bool> featureEnabled;
+    void addFeatureDescription(string key, 
+                               string cname,
+                               string humanReadable,
+                               string requires);
+    /** Initializes the on/off -toggled feature descriptions */
+    void initFeatureDescriptions();
+  public:
+    Features(){initFeatureDescriptions();}
+    std::vector<FeatureDescription>::iterator
+    begin(){return feats.begin();}
+    std::vector<FeatureDescription>::iterator
+    end(){return feats.end();}
+    void toStream(std::ostream &ost);
+  };
+
   /** Derive this to update GUI when a feature is turned on/off. */
   class FeatCallback {
   public:
@@ -53,92 +111,9 @@ namespace synti2base {
    */
   class PatchBank {
   private:
-    /** A template "default" patch. */
-    Patch defaultPatch;
-
-    std::vector<string> featkeys;
-    std::vector<FeatureDescription> feats;
-    std::map<string,bool> featureEnabled;
-
-    std::vector<string> capkeys;
-    std::vector<CapacityDescription> caps;
-    std::map<string,int> capValue;
-
+    Features feats;
+    Capacities caps;
     std::vector<Patch> patches;
-    void addFeatureDescription(string key, 
-                               string cname,
-                               string humanReadable,
-                               string requires)
-    {
-      featkeys.push_back(key);
-      featureEnabled[key] = false; // init everything to disabled.
-      feats.push_back(FeatureDescription(key, cname, humanReadable, requires));
-    }
-    void addCapacityDescription(string key, 
-                               string cname,
-                               string humanReadable,
-                               int min,
-                               int max,
-                               int initial,
-                               string requiresf
-                               )
-    {
-      capkeys.push_back(key);
-      capValue[key] = initial;
-      caps.push_back(CapacityDescription(key,cname,humanReadable,min,max,requiresf));
-    }
-
-    /** Initializes the on/off -toggled feature descriptions */
-    void initFeatureDescriptions(){
-      /*
-        These to be toggled by "COMPOSE_MODE" switch only:
-        
-      addFeatureDescription("sysex","FEAT_COMPOSE_MODE_SYSEX","Receive SysEx (usable in compose mode only)",""); // Need this at all?
-      addFeatureDescription("troubleshooting","FEAT_SAFETY","Safe mode (usable in compose mode only)","");
-      */
-      addFeatureDescription("fm","FEAT_APPLY_FM","Use frequency modulation","");
-      addFeatureDescription("add","FEAT_APPLY_ADD","Use addition matrix","");
-      addFeatureDescription("noise","FEAT_NOISE_SOURCE","Use noise source","");
-      addFeatureDescription("pdetune","FEAT_PITCH_DETUNE","Enable detuning of operators","");
-      /*
-      This won't be necessary in the final version. 1/10 cents is
-      enough accuracy for everybody. (or is it!? experience will tell.)
-
-      addFeatureDescription("pfine","FEAT_PITCH_FINE_DETUNE","Enable fine detune (convenience only!)","");
-      */
-      addFeatureDescription("delay","FEAT_DELAY_LINES","Enable delay effects","");
-      addFeatureDescription("squash","FEAT_OUTPUT_SQUASH","Squash function at output","");
-      addFeatureDescription("phase","FEAT_RESET_PHASE","Enable phase reset on note-on","");
-      addFeatureDescription("waves","FEAT_EXTRA_WAVETABLES","Generate extra wavetables with harmonics","");
-      addFeatureDescription("cube","FEAT_POWER_WAVES","Enable squared/cubed waveforms","");
-      addFeatureDescription("mods","FEAT_MODULATORS","Use modulators / parameter ramps","");
-      addFeatureDescription("pbend","FEAT_PITCH_BEND","Enable the 'pitch bend' modulator","mods");
-      addFeatureDescription("noff","FEAT_NOTE_OFF","Listen to note off messages","");
-      addFeatureDescription("velocity","FEAT_VELOCITY_SENSITIVITY","Respond to velocity","");
-      addFeatureDescription("looping","FEAT_LOOPING_ENVELOPES","Enable looping envelopes","");
-      addFeatureDescription("legato","FEAT_LEGATO","Enable legato","");
-      addFeatureDescription("pscale","FEAT_PITCH_SCALING","Enable pitch scaling","");
-      addFeatureDescription("filter","FEAT_FILTER","Enable the lo/band/hi-pass filter","");
-      addFeatureDescription("fres","FEAT_FILTER_RESO_ADJUSTABLE","Enable adjustable filter resonance","");
-      addFeatureDescription("fresenv","FEAT_FILTER_RESO_ENVELOPE","Enable resonance envelope","");
-      addFeatureDescription("fcutenv","FEAT_FILTER_CUTOFF_ENVELOPE","Enable cutoff envelope","");
-      addFeatureDescription("fpfollow","FEAT_FILTER_FOLLOW_PITCH","Enable filter frequency to follow pitch","");
-      addFeatureDescription("fnotch","FEAT_FILTER_OUTPUT_NOTCH","Enable notch filter","");
-      addFeatureDescription("csquash","FEAT_CHANNEL_SQUASH","Enable per-channel squash function","");
-      addFeatureDescription("stereo","FEAT_STEREO","Enable stereo output","");
-      addFeatureDescription("panenv","FEAT_PAN_ENVELOPE","Enable pan envelope","stereo");
-    }
-
-    /** Initializes integer-type capacities of the synth. */
-    void initCapacityDescriptions(){
-      addCapacityDescription("num_channels","NUM_CHANNELS","Number of channels (patches)",1,256,16,"");
-      addCapacityDescription("num_delays","NUM_DELAY_LINES","Number of delay lines",1,8,4,"delay");
-      addCapacityDescription("num_ops","NUM_ÖPERATORS","Number of operators/oscillators per channel", 0,4,4,"fm|add");
-      addCapacityDescription("num_envs", "NUM_ENVS", "Number of envelopes per channel", 1,6,2,"");
-      addCapacityDescription("num_knees","NUM_ENV_KNEES","Number of knees per envelope", 2,5,5,"");
-      addCapacityDescription("num_mods","NUM_MODULATORS","Number of controllable modulators per channel",0,4,4,"mods");
-    }
-
   public:
     PatchBank();
 
@@ -160,7 +135,7 @@ namespace synti2base {
     getCapacityEnd(){return caps.end();}
 
     int
-    getCapacityValue(string key){return capValue[key];}
+    getCapacityValue(string key){return caps.value(key);}
 
     void setNumPatches(int n);
     int getNumPatches(){return patches.size();}

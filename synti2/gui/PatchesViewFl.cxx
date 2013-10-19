049acdac2ffdd200903a197daff65701b6cfc94b
Paavo Nieminen <paavo.j.nieminen@jyu.fi>
2013-10-19 12:20:22 +0300

    Trying out the Code::Blocks IDE by renaming 'pbh' to 'pb' in one file.

--- synti2/gui/PatchesViewFl.cxx
+++ synti2/gui/PatchesViewFl.cxx
@@ -44,7 +44,7 @@ void cb_patch_name(Fl_Widget* w, void* p){
 /** Builds the patch editor widgets. */
   void ViewPatches::build_patch_editor(Fl_Group *gr, PatchBank *pbh)
 {
-  if (pbh==NULL) {
+  if (pb==NULL) {
     std::cerr << "Error: No PatchBank given. Can't build GUI." << std::endl;
     return;
   }
@@ -55,37 +55,37 @@ void cb_patch_name(Fl_Widget* w, void* p){
 
   patch->type(FL_SIMPLE_COUNTER);
   patch->align(FL_ALIGN_LEFT);
-  patch->bounds(0,pbh->getNumPatches()-1);
+  patch->bounds(0,pb->getNumPatches()-1);
   patch->precision(0);
-  patch->callback(cb_change_patch, pbh);
+  patch->callback(cb_change_patch, pb);
 
   Fl_Input * widget_patch_name = new Fl_Input(150,25,90,25,"Name");
-  widget_patch_name->callback(cb_patch_name,pbh);
+  widget_patch_name->callback(cb_patch_name,pb);
 
   int px=280, py=25, w=75, h=25, sp=2;
   int labsz = 16;
 
 
   Fl_Button *box = new Fl_Button(px+ 0*(w+sp),py,w,h,"S&end this");
-  //box->callback(cb_send_current); box->labelsize(labsz); 
+  //box->callback(cb_send_current); box->labelsize(labsz);
   //button_send_current = box;
 
   box = new Fl_Button(px + 1*(w+sp),py,w,h,"Send al&l");
-  //box->callback(cb_send_all); box->labelsize(labsz); 
+  //box->callback(cb_send_all); box->labelsize(labsz);
   //button_send_all = box;
 
   px += w/2;
   box = new Fl_Button(px + 2*(w+sp),py,w,h,"Save this");
-  //box->callback(cb_save_current); box->labelsize(labsz); 
+  //box->callback(cb_save_current); box->labelsize(labsz);
 
   box = new Fl_Button(px + 3*(w+sp),py,w,h,"&Save all");
-  //box->callback(cb_save_all); box->labelsize(labsz); 
+  //box->callback(cb_save_all); box->labelsize(labsz);
 
   box = new Fl_Button(px + 5*(w+sp),py,w,h,"Clear this");
-  //box->callback(cb_clear_current); box->labelsize(labsz); 
+  //box->callback(cb_clear_current); box->labelsize(labsz);
 
   box = new Fl_Button(px + 6*(w+sp),py,w,h,"Load this");
-  //box->callback(cb_load_current); box->labelsize(labsz); 
+  //box->callback(cb_load_current); box->labelsize(labsz);
 
   box = new Fl_Button(px + 7*(w+sp),py,w,h,"Load all");
   //box->callback(cb_load_all); box->labelsize(labsz);
@@ -99,12 +99,12 @@ void cb_patch_name(Fl_Widget* w, void* p){
   //FIXME: Include "type of GUI" and allowed values as attributes of
   //both the derived widget and I4Par description.
   /* Parameters Valuator Widgets */
-  for (i4it=pbh->getI4Begin(activePatch); 
-       i4it!=pbh->getI4End(activePatch);
+  for (i4it=pb->getI4Begin(activePatch);
+       i4it!=pb->getI4End(activePatch);
        ++i4it){
     // FIXME: Actual data from desription
     vi = new Fl_Value_Input(px+(i++)*(w+sp),py,w,h);
-    vi->tooltip(pbh->getI4Par(activePatch,*i4it).getHumanReadable().c_str());
+    vi->tooltip(pb->getI4Par(activePatch,*i4it).getHumanReadable().c_str());
     vi->precision(0);
     //vi->color(colortab[pd->getGroup("I3",i)]);
     //vi->argument(i); // what is da argument then?
@@ -112,21 +112,21 @@ void cb_patch_name(Fl_Widget* w, void* p){
   }
 
   py=80; w=85; h=15;
-  int npars = pbh->getFEnd(activePatch) - pbh->getFBegin(activePatch);
+  int npars = pb->getFEnd(activePatch) - pb->getFBegin(activePatch);
   int ncols = 4;
   int nrows = (npars / ncols) + 1;
   i = 0;
   int col=0,row=0;
 
-  for (fit=pbh->getFBegin(activePatch); 
-       fit!=pbh->getFEnd(activePatch);
+  for (fit=pb->getFBegin(activePatch);
+       fit!=pb->getFEnd(activePatch);
        ++fit){
     // FIXME: Actual data from desription
-      Fl_Roller *vsf = 
+      Fl_Roller *vsf =
         new Fl_Roller(px+col*250,py+row*(h+sp),w,h);
-      vsf->tooltip(pbh->getFPar(activePatch,*fit).getHumanReadable().c_str());
+      vsf->tooltip(pb->getFPar(activePatch,*fit).getHumanReadable().c_str());
       vsf->type(FL_HOR_NICE_SLIDER);
-      vsf->label(pbh->getFPar(activePatch,*fit).getHumanReadable().c_str());
+      vsf->label(pb->getFPar(activePatch,*fit).getHumanReadable().c_str());
       vsf->align(FL_ALIGN_RIGHT);
 
       row++;
@@ -138,11 +138,11 @@ void cb_patch_name(Fl_Widget* w, void* p){
     for (int row=0; row < nrows; row++){
       if (i==npars) break;
       /* Need to store all ptrs and have attach_to_values() */
-      Fl_Roller *vsf = 
+      Fl_Roller *vsf =
         new Fl_Roller(px+col*250,py+row*(h+sp),w,h);
       //widgets_f.push_back(vsf);
       /* FIXME: think? */
-      vsf->bounds(pd->getMin("F",i),pd->getMax("F",i)); 
+      vsf->bounds(pd->getMin("F",i),pd->getMax("F",i));
       vsf->precision(pd->getPrecision("F",i));
       vsf->color(colortab[pd->getGroup("F",i)]);
       vsf->label(createFvalLabel(i,pd->getDescription("F",i)).c_str());
@@ -160,7 +160,7 @@ void cb_patch_name(Fl_Widget* w, void* p){
 
 
 using namespace synti2gui;
-  ViewPatches::ViewPatches(int x, int y, int w, int h, const char * name, PatchBank *pbh) 
+  ViewPatches::ViewPatches(int x, int y, int w, int h, const char * name, PatchBank *pbh)
     : Fl_Group(x, y, w, h, name), activePatch(0)
 {
   pb = pbh;

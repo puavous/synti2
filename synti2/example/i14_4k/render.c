26563a1a1cf6d0f35c7690282f443e47f384b8e4
Paavo Nieminen <paavo.j.nieminen@jyu.fi>
2014-03-10 02:10:13 +0200

    Just a little bit smaller code for state collection and transfer.

--- synti2/example/i14_4k/render.c
+++ synti2/example/i14_4k/render.c
@@ -16,47 +16,40 @@
 /** Paint it. */
 static void render_scene(const synti2_synth *s){
   int i, j;
-
-  float cf;
-
   GLint unipar;
-
-
-  int isp;
+  GLfloat *isp;
 
   /* Global now:
      float synthtime;
      synthtime =  (float)(s->framecount) / s->sr;
   */
-
-  isp = 3;
-  
   //state[0] = synthtime;
   //state[1] = window_h*ar; /* globals */
   //state[2] = window_h;    /* globals */
 
-#if 0
+  isp = state + NUM_GLOBAL_PARAMS;
+  
   synti2_voice *v = s->voi;
   for(i=0; i<NUM_CHANNELS; i++){
     for(j=0; j<NUM_ENVS+1; j++)
-      state[isp++] = v->eprog[j].f;
+      *isp++ = v->eprog[j].f;
     for(j=0; j<NUM_MODULATORS; j++)
-      state[isp++] = v->contr[j].f;
-    state[isp++] = v->note;
+      *isp++ = v->contr[j].f;
+    *isp++ = v->note;
     v++;
   }
-#endif
 
+#if 0
   for(i=0; i<NUM_CHANNELS; i++){
     for(j=0;j<NUM_ENVS+1;j++){
-      state[isp++] = s->voi[i].eprog[j].f;
+      *isp++ = s->voi[i].eprog[j].f;
     }
     for(j=0;j<NUM_MODULATORS;j++){
-      state[isp++] = s->voi[i].contr[j].f;
+      *isp++ = s->voi[i].contr[j].f;
     }
-    state[isp++] = s->voi[i].note;    
+    *isp++ = s->voi[i].note;
   }
-
+#endif
 
   unipar = oglGetUniformLocation(pid, "s");
   oglUniform1fv(unipar, NUM_SYNTH_PARAMS, state);
@@ -64,23 +57,13 @@ static void render_scene(const synti2_synth *s){
   //  glEnable(GL_DEPTH_TEST);
   oglClear(GL_DEPTH_BUFFER_BIT|GL_COLOR_BUFFER_BIT);
 
-  //sivu(10,-2);
   glRects( -1, -1, 1, 1 );
-
 }
+
 
 /** Render something that varies with time and "audio snapshot". */
 static
 void render_w_shaders(const synti2_synth *s){
-
-  //oglMatrixMode(GL_PROJECTION);
-  //oglLoadIdentity();
-  /* The exe would be smaller, were screen size hard-coded.. 
-     .. but I try to be nice.. */
-  //oglFrustum(-ar,ar,-1.f,1.f,1.f,4.f);
-  //oglMatrixMode(GL_MODELVIEW);
-  //oglLoadIdentity();
   oglUseProgram(pid);
-
   render_scene(s);
 }

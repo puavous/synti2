88dc1e6118c65cb28fb4b29d4ab9577f1eb84e79
Paavo Nieminen <paavo.j.nieminen@jyu.fi>
2015-03-28 01:36:17 +0200

    Copy-paste of previous work. Why not..

---
+++ synti2/example/i15_4k/i15_4k.makefile
@@ -0,0 +1,32 @@
+# Customization for a final product. Can override any switches. Must
+# define at least CUSTOMFILES.
+
+# Assembly 2014 4k entry:
+# Specifics needed are song, sounds, and gfx:
+A14FILES = example/i14_4k/i14_4k.mid \
+	   example/i14_4k/i14_4k.s2bank \
+	   example/i15_4k/vertex.vert \
+	   example/i15_4k/fragment.frag \
+	   example/i15_4k/render.c \
+	   example/i15_4k/glfuncs.h \
+	   example/i15_4k/glfuncs.c
+
+CUSTOMFILES=$(A14FILES)
+
+CUSTOM_FLAGS=-DSCREEN_HEIGHT=80 -DPLAYBACK_DURATION=35
+
+## This gcc flag will use tinyexe.ld as the link script (..dangerous?):
+#		-Wl,-dTtinyexe.ld \
+
+## Uncomment to test without non-standard software dependencies:
+#GZIP_TMP_CMD = gzip -9 tmp
+#SHADER_TMP_CMD = sed -f src/shader_preview.sed > shader.tmp
+#SSTRIP=echo 'Not using sstrip!'
+
+# Final version benefits from zopfli compression:
+#GZIP_TMP_CMD = zopfli --i1000 tmp
+
+#Assembly compo rules were 32bit exe with forced resolution:
+#CUSTOM_FLAGS=-DSCREEN_HEIGHT=720 -DFULLSCREEN -DPLAYBACK_DURATION=35
+#ARCHFLAGS = -m32 -DNO_I64 `sdl-config --cflags`
+#ARCHLIBS = `sdl-config --libs` -lm /usr/lib/libGL.so.1
---
+++ synti2/example/i15_4k/render.c
@@ -0,0 +1,73 @@
+/** 
+ * Here are the rendering routines for my 4k intro production..  Uses
+ * the shaders in shader.c
+ *
+ * This will be just #included in the main file. I don't think it's
+ * "good practice" but I find it the simplest approach to get the
+ * thing compiled in small space.
+ */
+
+/* Some gfx cards don't allow very many uniform variables, so send
+ * fewer than available.. use the first few channels to control
+ * graphics...
+ */
+#define NUM_SYNTH_PARAMS_TRANSMITTED 200
+
+/** Paint it. */
+static void render_scene(const synti2_synth *s){
+  int i, j;
+  GLint unipar;
+  GLfloat *isp;
+
+  /*
+     compression    old  new
+     7za    -mx=9   4176 4244
+     zopfli --i25   4110 4128
+
+     Somehow this renderer gets compressed better
+     than the previous one, and now we get all
+     the envelopes, controllers, and notes for
+     all voices into the shader side.
+     
+  */
+
+  /* Global now:
+     float synthtime;
+     synthtime =  (float)(s->framecount) / s->sr;
+  */
+  //state[0] = synthtime;
+  //state[1] = window_h*ar; /* globals */
+  //state[2] = window_h;    /* globals */
+
+  isp = state + NUM_GLOBAL_PARAMS;
+  
+  const synti2_voice *v = s->voi;
+  for(i=0; i<NUM_CHANNELS; i++){
+    for(j=0; j<NUM_ENVS+1; j++)
+      *isp++ = v->c[CI_ENVS+j].f;
+    /* for consistency btw compose&playback*/
+    isp += (NUM_MAX_ENVS - NUM_ENVS);
+    for(j=0; j<NUM_MODULATORS; j++)
+      *isp++ = v->c[CI_MODS+j].f;
+    /* for consistency btw compose&playback*/
+    isp += (NUM_MAX_MODULATORS - NUM_MODULATORS);
+    *isp++ = v->note;
+    v++;
+  }
+
+  unipar = oglGetUniformLocation(pid, "s");
+  
+  oglUniform1fv(unipar, NUM_SYNTH_PARAMS_TRANSMITTED, state);
+
+  oglClear(GL_DEPTH_BUFFER_BIT|GL_COLOR_BUFFER_BIT);
+
+  oglRects( -1, -1, 1, 1 );
+}
+
+
+/** Render something that varies with time and "audio snapshot". */
+static
+void render_w_shaders(const synti2_synth *s){
+  oglUseProgram(pid);
+  render_scene(s);
+}

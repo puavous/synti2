1371eb820916cb4a3158b19a9746a8854b323142
Paavo Nieminen <paavo.j.nieminen@jyu.fi>
2015-07-29 13:00:46 +0300

    File was missing from repo.. maybe..

---
+++ synti2/example/default_defs.h
@@ -0,0 +1,13 @@
+/* Set default values to parameters. */
+#ifndef SAMPLE_RATE
+#define SAMPLE_RATE 48000
+#endif
+#ifndef AUDIO_BUFFER_SIZE
+#define AUDIO_BUFFER_SIZE 8192
+#endif
+#ifndef SCREEN_HEIGHT
+#define SCREEN_HEIGHT 108
+#endif
+#ifndef SCREEN_WIDTH
+#define SCREEN_WIDTH (SCREEN_HEIGHT * (16./9))
+#endif
